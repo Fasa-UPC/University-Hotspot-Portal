36be6ccf7dff91cce1bb223893f678bb51bad711
Max Edison <amirhossein.heidari.email@gmail.com>
2025-05-24 13:25:12 +0330

    fix(style & theme): fixed addressing

--- scripts/theme.js
+++ scripts/theme.js
@@ -11,12 +11,12 @@ let isdark = false;
 
 btn.addEventListener("click" , function(){
     if(isdark){
-        logo.setAttribute("src" , "../src/AvalGraphic.com-Fasa-University.svg");
-        svg.setAttribute("src" , "../src/Moon Stars.svg");
+        logo.setAttribute("src" , "src/AvalGraphic.com-Fasa-University.svg");
+        svg.setAttribute("src" , "src/Moon Stars.svg");
         isdark = false;
     }else{
-        logo.setAttribute("src" , "../src/FasaUniLogo1.svg");
-        svg.setAttribute("src" , "../src/Sun.svg");
+        logo.setAttribute("src" , "src/FasaUniLogo1.svg");
+        svg.setAttribute("src" , "src/Sun.svg");
         isdark = true
     }
 })
